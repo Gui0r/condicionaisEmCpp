21b03506b4fe55cc2210678f7c8e450d284d000c
Guilherme Michel <139629718+Gui0r@users.noreply.github.com>
2024-04-17 23:57:11 -0300

    Atualizar o calculoRec13.cpp

--- ExerciciosAula08abril/calculoRec13.cpp
+++ ExerciciosAula08abril/calculoRec13.cpp
@@ -1,46 +1,24 @@
 #include <iostream>
 #include <locale.h>
 #include <unistd.h>
+#include <string>
 
 using namespace std;
 
 int main(){
     setlocale(LC_ALL, "pt_BR.UTF-8");
 
-    int nota, MP, notaM = 10, notaRec, P;  
+    int a, b, c, P, media, MP;  
 
-    cout << "Digite sua nota: ";
-    cin >> nota;
-
-    cout << "Digite sua nota de recuperação: ";
-    cin >> notaRec;
-
-    MP = nota + notaRec;
+    cout << "Digite suas notas em sequência: ";
+    cin >> a;
+    cin >> b;
+    cin >> c;
 
     cout << "Digite sua presença em aula: ";
     cin >> P;
 
-    if(P >= 75 && MP < 10){
-        cout << "Reprovado!";
-    }else if(MP <= 10 || P < 75){
-        cout << "Reprovado!";
-    }else if (P >= 75 && MP >= 10){
-        cout << "Aprovado!";
-    }else{
-        cout << "Reprovado!";
-    }
-
-/*
-    if(MP < 10){
-        cout << "Reprovado!";
-    }else if(MP > 10){
-        cout << "Aprovado!";
-    }else{
-        cout << "Reprovado!";
-    }
-*/
-}
-/*
+    
     switch (media, MP)
     {
     case media > 7 && media < 5:
@@ -51,4 +29,3 @@ int main(){
         break;
     }
 }
-*/
